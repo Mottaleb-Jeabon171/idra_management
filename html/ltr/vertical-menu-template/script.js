eee779d0b9229fa783d0382aed67ad8a045e1fd5
Mottaleb-Jeabon171 <mottaleb.jebon@gmail.com>
2023-12-06 13:30:23 +0600

    first commit

---
+++ html/ltr/vertical-menu-template/script.js
@@ -0,0 +1,48 @@
+
+const objectListee = [
+
+  {
+    id: 1, name: "Gross Premium", previousyear: "",
+    listadd: ["Fire", "Marine (Cargo)", "Marine (Hull)", "Motor", "Micellaneous"]
+  },
+
+  {
+    id: 2, name: "Re-Insurance Premium paid", previousyear: "",
+    listadd: ["Fire", "Marine (Cargo)", "Marine (Cargo)", "Marine (Cargo)", "Marine (Hull)", "Motor", "Micellaneous"]
+  },
+
+  {
+    id: 3, name: "Net Premium (1-2)", previousyear: "",
+    listadd: ["Fire", "Marine (Cargo)", "Marine (Cargo)", "Marine (Cargo)", "Marine (Hull)", "Motor", "Micellaneous"]
+  },
+
+
+  {
+    id: 4, name: "Re-Insurance Premium payable of Sadharan Bima", previousyear: "",
+    listadd: ["Fire", "Marine (Cargo)", "Marine (Cargo)", "Marine (Cargo)", "Marine (Hull)", "Motor", "Micellaneous"]
+  },
+
+
+  {
+    id: 5, name: "Gross Premium", previousyear: "",
+    listadd: ["Fire", "Marine (Cargo)", "Marine (Cargo)", "Marine (Cargo)", "Marine (Hull)", "Motor", "Micellaneous"]
+  },
+
+  {
+    id: 6, name: "Gross Premium", previousyear: "",
+    listadd: ["Fire", "Marine (Cargo)"]
+  }
+]
+
+
+    const headerlistee = ["Previous Year(2022)", "January-March-2023(1st-Q)", "April-June-2023 (2nd-Q)", "July-September-2023(3rd-Q)", "October-December-2023(4th-Q)", "Current-Year(2023)"];
+
+
+
+// <th class="text-gray-500 text-center bg-gray-50 border border-gray-300"></th>
+//               <th class="text-gray-500 text-center bg-gray-50 border border-gray-300"></th>
+//               <th class="text-gray-500 text-center bg-gray-50 border border-gray-300"></th>
+//               <th class="text-gray-500 text-center bg-gray-50 border border-gray-300"></th>
+//               <th class="text-gray-500 text-center bg-gray-50 border border-gray-300">
+//               </th>
+//               <th class="text-gray-500 text-center bg-gray-50 border border-gray-300"></th>
